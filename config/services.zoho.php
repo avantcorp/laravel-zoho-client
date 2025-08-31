62330f6cb28de87c51dbd7ffca11b89310df7f84
Ali Saleem <alisaleem@outlook.com>
2025-08-31 20:21:42 +0100

    update Zoho config to use environment variables

--- config/services.zoho.php
+++ config/services.zoho.php
@@ -3,8 +3,8 @@
 return [
     'client_id'           => env('ZOHO_CLIENT_ID'),
     'client_secret'       => env('ZOHO_CLIENT_SECRET'),
-    'scopes'              => [],
-    'token_storage_path'  => storage_path('app/zoho-token.json'),
-    'redirect_on_success' => '/',
-    'web_authentication'  => false,
+    'scopes'              => array_map(fn ($scope) => trim($scope), array_filter(explode(',', env('ZOHO_SCOPES')))),
+    'token_storage_path'  => env('ZOHO_TOKEN_STORAGE_PATH', storage_path('app/zoho-token.json')),
+    'redirect_on_success' => env('ZOHO_REDIRECT_ON_SUCCESS', '/'),
+    'web_authentication'  => env('ZOHO_WEB_AUTHENTICATION', false),
 ];
